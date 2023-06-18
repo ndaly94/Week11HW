d8c4f37a8f6fc367db0f82f3ffc503926bb7804d
NDaly94 <NDaly94@gmail.com>
2023-06-18 14:54:21 -0400

    Recursion Updated

--- theoretical/recursion.js
+++ theoretical/recursion.js
@@ -56,21 +56,60 @@ function fibonacci(n) {
   return fibonacci(n-1) + fibonacci(n-2);
 }
 
-function coinFlips() {
+function coinFlips(n) {
   // This function returns an array of all possible outcomes from flipping a coin N times.
   // Input type: Integer
   // For example, coinFlips(2) would return the following:
   // ["HH", "HT", "TH", "TT"]
   // H stands for Heads and T stands for tails
   // Represent the two outcomes of each flip as "H" or "T"
+  if (n === 0) {
+    return [];
+  }
+
+  // Base case: If n is 1, return the array with two outcomes: ["H", "T"]
+  if (n === 1) {
+    return ["H", "T"];
+  }
+
+  // Recursive case: Generate combinations for n-1 flips
+  const prevCombinations = letterCombinations(n - 1);
+
+  // Generate new combinations by appending "H" and "T" to each previous combination
+  const newCombinations = [];
+  for (const combination of prevCombinations) {
+    newCombinations.push(combination + "H");
+    newCombinations.push(combination + "T");
+  }
+
+  return newCombinations;
 }
 
-function letterCombinations() {
+
+function letterCombinations(letters) {
   // This function returns an array of all combinations of the given letters
   // Input type: Array of single characters
   // For example, letterCombinations(["a","b","c"]) would return the following:
   // ["a","b","c","ab","ac","ba","bc","ca","cb","abc","acb","bac","bca","cab","cba"]
+  if (letters.length === 0) {
+    return [];
+  }
+
+  const combinations = [];
+
+  function generateCombos(currentCombo, remainingLetters) {
+    combinations.push(currentCombo);
+
+    for (let i = 0; i < remainingLetters.length; i++) {
+      generateCombos(currentCombo + remainingLetters[i], remainingLetters.slice(i + 1));
+    }
+  }
+
+  generateCombos("", letters);
+
+  return combinations;
 }
+
 
 module.exports = {
   lengthOfString,
